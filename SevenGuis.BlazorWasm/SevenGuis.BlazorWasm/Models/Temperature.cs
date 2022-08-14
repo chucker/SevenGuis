947a43019ae4e9f991c1410999dd2069c5536a62
Sören Kuklau <kuklau@trenz.de>
2022-08-14 21:45:37 +0200

    Temperature Converter in Blazor

---
+++ SevenGuis.BlazorWasm/SevenGuis.BlazorWasm/Models/Temperature.cs
@@ -0,0 +1,19 @@
+﻿public class Temperature
+{
+    private float _Celsius = 0;
+
+    public float Celsius
+    {
+        get => _Celsius;
+        set => _Celsius = value;
+    }
+
+    public float Fahrenheit
+    {
+        get => (_Celsius * 9 / 5) + 32;
+        set
+        {
+            _Celsius = (value - 32) * 5 / 9;
+        }
+    }
+}
